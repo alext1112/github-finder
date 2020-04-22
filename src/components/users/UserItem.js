655fbed6ca356e6b0ead505abdbc0f4db75cc20d
Alex Travick <alextravick18@gmail.com>
2020-04-21 23:52:25 -0400

    refactored UserItem

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,30 +1,28 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-  constructor(props) {
-    super();
-    this.state = {
+  state = {
       id: 'id',
       login: 'mojombo',
       avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
       html_url: 'https://github.com/mojombo'
     }
-  }
 
 
 
   render() {
+    const { login, avatar_url, html_url } = this.state;
     return (
       <div className="card text-center">
-        <img src={this.state.avatar_url} className="round-img" style={{
+        <img src={avatar_url} className="round-img" style={{
           width: '60px'
         }} />
-        <h3>{this.state.login.toUpperCase()}</h3>
+        <h3>{login.toUpperCase()}</h3>
         <div>
           <a
           style={{
             borderRadius: '10px'
-          }} href={this.state.html_url} className="btn btn-dark btn-sm my-1">More</a>
+          }} href={html_url} className="btn btn-dark btn-sm my-1">More</a>
         </div>
       </div>
     )
